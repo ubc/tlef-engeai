19a7fbf7310d05e30d407de4ddb5ecebce51da2d
gatahcha <charismaformal@gmail.com>
2025-08-18 10:45:37 -0700

    problem solved : has not implement sliding css

--- public/scripts/instructor-mode.ts
+++ public/scripts/instructor-mode.ts
@@ -55,7 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     attachWelcomeScreenListeners();
                 })
             }
-
             renderFeatherIcons();
 
         }
